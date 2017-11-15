326ca92b569d128c42e4319b5b6e9e799b54092a
Dyan Carra <dyancarra@gmail.com>
2017-11-15 14:01:10 -0200

    Minors

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :root, File.dirname(__FILE__)
   set :views, File.expand_path("#{root}/../views", __FILE__)
-  set :public_folder, 'app/public'
 
   get '/' do
     erb :index
