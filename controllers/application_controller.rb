99a827d58ef616f08c3a6f122988167fa2882795
Dyan Carra <dyancarra@gmail.com>
2017-11-15 13:17:46 -0200

    add some css

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  set :views, File.expand_path("#{root}/../views", __FILE__)
+  set :public_folder, 'app/public'
+
   get '/' do
-    "Hello World!"
+    erb :index
   end
 end
