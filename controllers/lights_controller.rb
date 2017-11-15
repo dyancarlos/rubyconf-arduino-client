74806083194cdb6893f985fba9d9ef738dd24a11
Dyan Carra <dyancarra@gmail.com>
2017-11-15 13:18:44 -0200

    add redirect light controller

--- controllers/lights_controller.rb
+++ controllers/lights_controller.rb
@@ -1,9 +1,11 @@
 class LightsController < ApplicationController
   get '/on' do
     Pusher.trigger('light', 'change-status', "on")
+    redirect "/"
   end
 
   get '/off' do
     Pusher.trigger('light', 'change-status', "off")
+    redirect "/"
   end
 end
